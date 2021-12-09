d19fab62f549f5484874290d7823b7c138724be8
murilobracero <murilobracero@gmail.com>
2021-12-09 12:11:09 -0300

    SEC-FIX:removido acesso explicito ao banco de dados

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
 
+if(process.env.NODE_ENV === 'local'){
+    require('dotenv').config()
+}
+
 //iniciando o servidor
 const app = express();
 app.use(express.json());
@@ -10,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 //iniciando o db
-mongoose.connect('mongodb+srv://admin:admin@leak-of-legends-kmcv7.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useCreateIndex: true }).catch(err => console.log(`Erro ao conectar: ${err}`));
 requireDir('./src/Models');
 
